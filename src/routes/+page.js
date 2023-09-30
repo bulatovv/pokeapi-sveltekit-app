fd34cb971995eb373f1213f502d23999a779f75f
Albert Fazlyev <albert.fz@yandex.ru>
2023-09-30 09:52:15 +0500

    init repo

---
+++ src/routes/+page.js
@@ -0,0 +1,27 @@
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+    const chunkSize = 400;
+    let endpoint = `https://pokeapi.co/api/v2/pokemon?limit=${chunkSize}`;
+
+    const pokemons = [];
+
+    while (endpoint) {
+        const response = await fetch(endpoint);
+        const chunk = await response.json();
+
+        for (const pokemon of chunk.results) {
+            const id = pokemon.url.split('/').slice(-2)[0];
+            pokemon.id = id;
+        }
+
+        pokemons.push(...chunk.results);
+
+        endpoint = chunk.next;
+    }
+
+    console.log(pokemons);
+
+    return {
+        pokemons: pokemons
+    };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<main>
+    {#each data.pokemons as pokemon (pokemon.name)}
+        <a href="pokemon/{pokemon.id}">
+            <div class="pokemon">
+                <!-- show spinner while loading image -->
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon.id}.png" alt="{pokemon.name}" />
+                <h3>{pokemon.name}</h3>
+            </div>
+        </a>
+    {/each}
+
+</main>
