61f01a080684464e7aa682547950896aa0f06795
Albert Fazlyev <albert.fz@yandex.ru>
2023-09-30 12:02:42 +0500

    move pokemon card to separate component

---
+++ src/lib/components/PokemonCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let pokemon;
+</script>
+
+
+<div class="rounded-lg bg-gray-100 p-4 m-4 flex flex-col items-start shadow-md">
+    <img 
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/{pokemon.id}.png" alt="[{pokemon.name}]" 
+        class="w-72 h-72"
+    />
+    <h3>{pokemon.name}</h3>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-	/** @type {import('./$types').PageData} */
+    import PokemonCard from '$lib/components/PokemonCard.svelte';
+
+    /** @type {import('./$types').PageData} */
 	export let data;
     
     let search = '';
@@ -8,17 +10,23 @@
         pokemon => pokemon.name.startsWith(search)
     );
 
+    
+
 </script>
 
 <main>
-    <input type="text" bind:value={search} placeholder="Search..." class="rounded-lg bg-gray-100 p-4 m-4" />
+    <input 
+        type="text"
+        bind:value={search}
+        placeholder="Search..."
+        class="rounded-lg bg-gray-100 p-4 m-4 shadow-md" />
+
+    <div class="grid grid-cols-3 gap-1">
+        {#each filtered as pokemon (pokemon.name)}
+            <PokemonCard {pokemon} />
+        {/each}
+    </div>
 
 
-    {#each filtered as pokemon (pokemon.name)}
-        <div class="rounded-lg bg-gray-100 p-4 m-4 flex flex-col items-start">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon.id}.png" alt="[{pokemon.name}]" />
-            <h3>{pokemon.name}</h3>
-     </div>
-    {/each}
 
 </main>
