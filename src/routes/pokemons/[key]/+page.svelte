03f6883fafe9558fd4301c3cf5db42019442ac82
Albert Fazlyev <albert.fz@yandex.ru>
2023-10-27 21:28:32 +0500

    add fight and fast fight buttons

--- src/routes/pokemons/[key]/+page.svelte
+++ src/routes/pokemons/[key]/+page.svelte
@@ -2,8 +2,6 @@
     import type { PageData } from './$types';
     
     export let data: PageData;
-    // show pokemon info: name, stats.hp, stats.attack
-
 </script>
 
 <div class="flex flex-row items-center justify-start bg-gray-100 rounded-lg shadow-md mb-5">
@@ -12,18 +10,22 @@
          class="w-96 h-96" />
 
     <div class="flex flex-col items-start">
-        <h1 class="text-2xl font-bold">{data.pokemon.name}</h1>
+        <h1 class="text-2xl font-bold capitalize">{data.pokemon.name}</h1>
         <p class="text-lg font-semibold">HP: {data.pokemon.stats.hp}</p>
         <p class="text-lg font-semibold">Attack: {data.pokemon.stats.attack}</p>
     </div>
 </div>
     
-<div class="flex flex-row justify-around">
-    <a href="/" class="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+<div class="flex flex-row">
+    <a href="/" class="ml-4 mr-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         &larr; Back
     </a>
 
+    <a href="" class="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Quick fight ⏳
+    </a>
+
     <a class="ml-4 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-        Fight &rarr;
+        Fight ⚔️
     </a>
 </div>
