88c6f5233285c31fecb7e7e6943e86269e409a23
Albert Fazlyev <albert.fz@yandex.ru>
2023-10-27 15:11:02 +0500

    add support to get pokemon page by id

--- src/routes/pokemons/[key]/+page.ts
+++ src/routes/pokemons/[key]/+page.ts
@@ -1,10 +1,15 @@
 import type { PageLoad } from './$types';
 import type { PokemonWithStats } from '$lib/dto/pokemon-with-stats';
 import { GetPokemonByName } from '$lib/queries/get-pokemon-by-name';
+import { GetPokemonById } from '$lib/queries/get-pokemon-by-id';
 
 export const load: PageLoad = async ({ params }) => {
+    
+    const isNumerical = /^\d+$/.test(params.key);
 
-    const pokemon: PokemonWithStats = await new GetPokemonByName(params.pokemon).execute();
+    const pokemon: PokemonWithStats = isNumerical 
+        ? await new GetPokemonById(parseInt(params.key)).execute()
+        : await new GetPokemonByName(params.key).execute()
 
     return {
         pokemon
