5641db81c48f04a51c39983a25c6c1f318c9722a
Albert Fazlyev <albert.fz@yandex.ru>
2023-10-27 13:11:33 +0500

    move pokemons indedx fetch to separate query class

---
+++ src/lib/queries/search-pokemons-by-name.ts
@@ -0,0 +1,109 @@
+import { pokeapiGraphqlEndpoint } from '../config/services';
+
+type PokemonWithStats = {
+    id: number;
+    name: string;
+    stats: {hp: number; attack: number};
+};
+
+
+type PokemonsWithStatsPaginated = {
+    pokemons: PokemonWithStats[];
+    page: number;
+    perPage: number;
+    totalPages: number;
+};
+
+export class SearchPokemonsByName {
+    constructor(
+        protected readonly search: string,
+        protected readonly page: number,
+        protected readonly perPage: number,
+    ) {}
+
+    async execute(): Promise<PokemonsWithStatsPaginated> {
+
+        const request = await fetch(pokeapiGraphqlEndpoint, {
+            method: 'POST',
+            body: JSON.stringify({
+                query: `
+                    query($search: String!, $limit: Int!, $offset: Int!) {
+                        species: pokemon_v2_pokemon(where: {name: {_ilike: $search}}, limit: $limit, offset: $offset) { 
+                            id,
+                            name,
+                            pokemon_v2_pokemonstats(where: {pokemon_v2_stat: {name: {_in: ["hp", "attack"]}}}) {
+                                base_stat
+                                pokemon_v2_stat {
+                                    name
+                                }
+                            }
+                        }
+                        aggregate: pokemon_v2_pokemonspecies_aggregate(where: {name: {_ilike: $search}}) {
+                            aggregate {
+                                count
+                            }
+                        }
+                    }
+
+                `,
+                variables: { 
+                    search: `${this.search}%`,
+                    limit: this.perPage, 
+                    offset: (this.page - 1) * this.perPage 
+                },
+            })
+        });
+        
+        type GraphqlResponse = {
+            data: {
+                species: {
+                    id: number;
+                    name: string;
+                    pokemon_v2_pokemonstats: {
+                        base_stat: number;
+                        pokemon_v2_stat: {
+                            name: string;
+
+                        }
+                    }[];
+                }[];
+                aggregate: {
+                    aggregate: {
+                        count: number;   
+                    }
+                }
+            }
+        }
+
+
+
+        const response: GraphqlResponse = await request.json();
+
+        const pokemons = response.data.species.map((pokemon) => {
+            const stats: {hp: number; attack: number} = {hp: 0, attack: 0};
+            
+            pokemon.pokemon_v2_pokemonstats.forEach((stat) => {
+                if (stat.pokemon_v2_stat.name === 'hp') {
+                    stats.hp = stat.base_stat;
+                } else if (stat.pokemon_v2_stat.name === 'attack') {
+                    stats.attack = stat.base_stat;
+                }
+            });
+
+            return {
+                id: pokemon.id,
+                name: pokemon.name,
+                stats,
+            };
+        });
+
+        return {
+            pokemons,
+            page: this.page,
+            perPage: this.perPage,
+            totalPages: Math.ceil(response.data.aggregate.aggregate.count / this.perPage),
+        };
+
+    }
+}
+
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,42 +1,14 @@
-import { pokeapiGraphqlEndpoint } from '$lib/config/services';
 import type { PageLoad } from './$types';
+import { SearchPokemonsByName } from '$lib/queries/search-pokemons-by-name';
+
 
 export const load: PageLoad = async ({ url }) => {
     const search = url.searchParams.get('search') || '';
     const page = parseInt(url.searchParams.get('page') || '1');
     const perPage = 12;
+ 
+    const { pokemons, totalPages } = await new SearchPokemonsByName(search, page, perPage).execute();
 
-    const request = await fetch(pokeapiGraphqlEndpoint, {
-        method: 'POST',
-        body: JSON.stringify({
-            query: `
-                query($search: String!, $limit: Int!, $offset: Int!) {
-                    species: pokemon_v2_pokemonspecies(where: {name: {_ilike: $search}}, limit: $limit, offset: $offset) { 
-                        id,
-                        name,
-                    }
-                    aggregate: pokemon_v2_pokemonspecies_aggregate(where: {name: {_ilike: $search}}) {
-                        aggregate {
-                            count
-                        }
-                    }
-                }
+    return { search, page, pokemons, totalPages };
 
-            `,
-            variables: { 
-                search: `${search}%`,
-                limit: perPage, 
-                offset: (page - 1) * perPage 
-            },
-        })
-    });
-
-    const response = await request.json();
-
-    return {
-        search: search,
-        page: page,
-        pokemons: response.data.species,
-        totalPages: Math.ceil(response.data.aggregate.aggregate.count / perPage),
-    };
 }
